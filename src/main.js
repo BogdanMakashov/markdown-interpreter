b84fed205128829140503de8e7bddee1c54fc33e
Bogdan Makashov <bione1993@gmail.com>
2025-03-02 20:21:59 +0500

    Add support of primary markdown's elements

--- src/main.js
+++ src/main.js
@@ -1,20 +1,53 @@
 import './style.css';
 
-const boldRegexp = /\*\*(.+?)\*\*(?!\*)/gm;
+const possibleElements = [
+	{
+		name: 'Heading',
+		regexp: /#{1,5}\s(.+)/gm,
+		replacer: (text) => `<h1>${text}</h1><hr>`,
+	},
+	{
+		name: 'Bold',
+		regexp: /\*\*(.+?)\*\*(?!\*)/gm,
+		replacer: (text) => `<strong>${text}</strong>`,
+	},
+	{
+		name: 'Italic',
+		regexp: /\*(.+?)\*(?!\*)/gm,
+		replacer: (text) => `<i>${text}</i>`,
+	},
+	{
+		name: 'Blockquote',
+		regexp: /&gt; (.+)/gm,
+		replacer: (text) => `<blockquote>${text}</blockquote>`,
+	},
+	{
+		name: 'Code',
+		regexp: /\`(.+?)\`(?!\`)/gm,
+		replacer: (text) => `<code>${text}</code>`,
+	},
+];
 
 document.addEventListener('DOMContentLoaded', () => {
 	const meEditor = document.querySelector('.markdown-container');
 	const meEditorOutput = document.querySelector('.markdown-output');
 
 	meEditor.addEventListener('input', (event) => {
-		const initialContent = event.target.textContent;
+		const initialContent = event.target.innerHTML;
 		let outputContent = initialContent;
-		const boldMatches = initialContent.match(boldRegexp) || [];
 
-		if (boldMatches.length) {
-			outputContent = initialContent.replace(boldRegexp, (_, p1) => {
-				return `<strong>${p1}</strong>`;
-			});
+		for (const markdownElement of possibleElements) {
+			const matches = initialContent.match(markdownElement.regexp) || [];
+
+			if (matches.length) {
+				outputContent = outputContent.replace(markdownElement.regexp, (_, p1) => {
+					return markdownElement.replacer(p1);
+				});
+			}
+		}
+
+		if (event.inputType === 'insertLineBreak') {
+			outputContent += '<br>';
 		}
 
 		meEditorOutput.innerHTML = outputContent;
